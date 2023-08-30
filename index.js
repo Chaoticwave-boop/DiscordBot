325fefc389a70c13091df5dd40091f3036a5b875
CedricPedric <cedricnelson03@gmail.com>
2023-08-30 14:51:19 +0200

    /games command can take inputs not ready yet tho

--- index.js
+++ index.js
@@ -10,18 +10,33 @@ const client = new Discord.Client({ intents: [
 
   ]})
 
+  let gamelist = [];
 
   client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
 
   client.on('messageCreate', async (message) => {
-    console.log(message.content)
+    // console.log(message)
     if (message.content == "ping"){
-      message.reply("FUCKING PONG!")
+      message.reply("pong")
     }
- 
-  });
+    else if (message.content == "Hi"){
+      message.reply(`Hello ${message.author.globalName}!`)
+    }
+    if (message.content.includes("/ping")){
+      message.reply("/pong")
+    }
+    if (message.content.includes("/games")){
+      let games = (message.content.split(/[:,,]/))
+      games.splice(games.indexOf('/games'), 1)
+      console.log(games)
+      message.reply(`these are the games  you chose: ${games.toString(" ")}` )
+    }
+    })
+
+    
+
 
   
 //make sure this line is the last line
