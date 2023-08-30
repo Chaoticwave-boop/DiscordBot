80d567c5859b6c3cddd73c18debe5a712375dfa9
CedricPedric <cedricnelson03@gmail.com>
2023-08-30 13:28:28 +0200

    the bot replies

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ const Discord = require('discord.js'); //import discord.js
 
 const client = new Discord.Client({ intents: [
     Discord.GatewayIntentBits.Guilds,
-    Discord.GatewayIntentBits.GuildMessages
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent,
+    
+
+
   ]})
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
   });
 
+  client.on('messageCreate', async (message) => {
+    console.log(message.content)
+    if (message.content == "ping"){
+      message.reply("FUCKING PONG!")
+    }
+ 
+  });
+
+  
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using token
-
